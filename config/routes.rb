be75d0d1ce64b7750e73847b28496a9531383d75
mzamoranoc <mzamoran@chicagobooth.edu>
2025-03-27 01:07:39 +0000

    inclass2007

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  resources :books
+  # Routes for the Movie resource:
+  resources :movies
+
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })  
+  
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:id", { :controller => "movies", :action => "show" })
+
+  
+  
+  # UPDATE
+  get("/movies/:id/edit", {controller: "movies", action: "edit"})
+
+  patch("/movies/:id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
